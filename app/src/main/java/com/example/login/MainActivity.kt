131a254c56057f656bd2ce21054144b1b80bec2c
Eemeli Hayrynen <emppuposti@outlook.com>
2023-01-26 12:43:50 +0200

    0.2

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val signUp = findViewById<TextView>(R.id.signUp)
+        val logIn = findViewById<TextView>(R.id.logIn)
+        val sign = findViewById<LinearLayout>(R.id.sign)
+        val signIn = findViewById<Button>(R.id.signIn)
+        val logInLayout = findViewById<LinearLayout>(R.id.logInLayout)
+        signUp.setOnClickListener{
+            signUp.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            signUp.setTextColor(resources.getColor(R.color.textColor,null))
+            logIn.background = null
+            sign.visibility = View.VISIBLE
+            logInLayout.visibility = View.GONE
+            logIn.setTextColor(resources.getColor(R.color.pinkColor,null))
+        }
+        logIn.setOnClickListener{
+            signUp.background = null
+            signUp.setTextColor(resources.getColor(R.color.pinkColor,null))
+            logIn.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            sign.visibility = View.GONE
+            logInLayout.visibility = View.VISIBLE
+            logIn.setTextColor(resources.getColor(R.color.white,null))
+        }
+        signIn.setOnClickListener{
+            startActivity(Intent(this@MainActivity,NewActivity::class.java))
+        }
     }
 }
